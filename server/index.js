da7550f3d5381ada6f531d064ea8f589dd48565f
daniel.fernandez_ext@airenetworks.es <yerhods@gmail.com>
2024-06-10 15:20:35 +0200

    hhhhhh

--- server/index.js
+++ server/index.js
@@ -41,9 +41,10 @@ nextApp
 	.prepare()
 	.then(() => {
 		app.set('view engine', 'html');
-		app.use(express.json({ limit: '10mb', extended: true }));
+		app.use(express.json({ limit: '100mb', extended: true }));
+		app.use(express.urlencoded({ extended: true }));
 		app.use(cors());
-		//app.use(allowCrossDomain);
+		app.use(allowCrossDomain);
 		app.use(cookieParser());
 		app.get('/sign', (req, res) => {
 			const data = {
@@ -94,20 +95,8 @@ nextApp
 			res.send(data)	
 		});
 		app.post('/relay', (req, res) => {
-			const data = {
-				type: "POST",
-				query: JSON.stringify(req.query),
-				cookies: JSON.stringify(req.cookies),
-				headers: JSON.stringify(req.headers),
-				body: JSON.stringify(req.body),
-				params: JSON.stringify(req.params),
-				url: JSON.stringify(req.url),
-				originalUrl: JSON.stringify(req.originalUrl),
-				baseUrl: JSON.stringify(req.baseUrl),
-				path: JSON.stringify(req.path),
-				hostName: JSON.stringify(req.hostname),
-			}
-			res.send(data)	
+			
+			res.send(JSON.stringify(req))	
 		});
 		app.post('/reply', (req, res) => {
 			const data = {
